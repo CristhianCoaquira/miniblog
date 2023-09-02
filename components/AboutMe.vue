210fd2fe61292a24615dcbde83e31ec6c41185ee
DTK <cristhiandanielcoaquira@gmail.com>
2023-09-02 14:20:18 -0400

    Upgrade packages to work with sass @apply

---
+++ components/AboutMe.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about-me">About Me</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/ArticleCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="article-card">Article Card</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.article-card {
+  @apply border border-red-700;
+}
+</style>
